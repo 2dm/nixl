984ffb6a9362e9b699c67dc3e8fffa73b02ad0ad
Itay Alroy <ialroy@nvidia.com>
2025-11-09 23:24:40 +0200

    Add license headers to all files
    Signed-off-by: Itay Alroy <ialroy@nvidia.com>

--- examples/cpp/nixl_ep/tests/elastic/rank_server.py
+++ examples/cpp/nixl_ep/tests/elastic/rank_server.py
@@ -1,14 +1,24 @@
-# rank_server_full.py
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from socketserver import ThreadingTCPServer, StreamRequestHandler
 from threading import Thread, Lock
 from collections import defaultdict
 import socket, os
 import torch
 import time
-
-# --- Server state ---
-
-
 
 # --- Request handler ---
 class H(StreamRequestHandler):
