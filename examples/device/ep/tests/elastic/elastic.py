336554af5ebc0ff60dc158960917b6c3783d0e94
Micha Dery <mdery@nvidia.com>
2026-01-15 09:04:30 -0800

    Porting internode test from DeepEP to NixlEP
    Co-authored-by: Roey Azran <roeya@nvidia.com>

--- examples/device/ep/tests/elastic/elastic.py
+++ examples/device/ep/tests/elastic/elastic.py
@@ -492,10 +492,12 @@ def worker(torch_rank: int, args: argparse.Namespace):
         explicitly_destroy=True,
         enable_shrink=True,
         tcp_store_group=tcp_store,
+        low_latency_mode=True,
     )
     buffer.update_memory_buffers(
         num_ranks=max_num_ranks,
         num_experts_per_rank=args.num_experts_per_rank,
+        num_nvl_bytes=0,
         num_rdma_bytes=num_rdma_bytes,
     )
     signal.signal(
